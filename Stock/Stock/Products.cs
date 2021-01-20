a23cbdb013676e4607d0a1eb96c943dec6e937e2
Hw Ooi <hanweiooi97@gmail.com>
2021-01-20 08:45:35 +0800

    Added Stock Project Source Code

---
+++ Stock/Stock/Products.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Stock
+{
+    public partial class Products : Form
+    {
+        public Products()
+        {
+            InitializeComponent();
+        }
+
+        private void Products_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            LoadData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Stock;Integrated Security=True");
+            //Insert Logic
+            con.Open();
+            bool status = false;
+            if(comboBox1.SelectedIndex == 0)
+            {
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+
+            //check if product record is exist
+            var sqlQuery = ""; //initialize sqlQuery
+            if (IfProductsExists(con, textBox1.Text))
+            {
+                sqlQuery = @"UPDATE [dbo].[Products] SET [ProductName] = '" + textBox2.Text + "' ,[ProductStatus] = '" + status + "' WHERE [ProductCode] = '" + textBox1.Text + "'";
+            }
+            else
+            {
+                sqlQuery = @"INSERT INTO[dbo].[Products]([ProductCode],[ProductName],[ProductStatus])
+            VALUES ('" + textBox1.Text + "','" + textBox2.Text + "','" + status + "')";
+            }
+
+            SqlCommand cmd = new SqlCommand(sqlQuery, con);
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+            LoadData();
+           
+        }
+        private bool IfProductsExists(SqlConnection con, string productCode)
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("Select 1 From [dbo].[Products] WHERE [ProductCode]='"+ productCode +"'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count > 0)
+                return true;
+            else
+                return false;
+        }
+        public void LoadData()
+        {
+            //Reading Data
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Stock;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter("Select * From [dbo].[Products]", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.Rows.Clear();
+            foreach (DataRow item in dt.Rows)
+            {
+                int n = dataGridView1.Rows.Add();
+                dataGridView1.Rows[n].Cells[0].Value = item["ProductCode"].ToString();
+                dataGridView1.Rows[n].Cells[1].Value = item["ProductName"].ToString();
+                if ((bool)item["ProductStatus"])
+                {
+                    dataGridView1.Rows[n].Cells[2].Value = "Active";
+                }
+                else
+                {
+                    dataGridView1.Rows[n].Cells[2].Value = "Deactive";
+                }
+
+            }
+        }
+
+        private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            comboBox1.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Stock;Integrated Security=True");
+            var sqlQuery = ""; //initialize sqlQuery
+            if (IfProductsExists(con, textBox1.Text))
+            {
+                con.Open();
+                sqlQuery = @"DELETE from [dbo].[Products] WHERE [ProductCode] = '" + textBox1.Text + "'";
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+            }
+            else
+            {
+                MessageBox.Show("Record not exist...");
+            }
+
+
+
+            LoadData();
+        }
+    }
+}
